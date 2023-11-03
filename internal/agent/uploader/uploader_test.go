66dd95fb297c70bd10c0a3244f9d3260392840ea
e.tsibereva <e.tsibereva@tinkoff.ru>
2023-11-03 14:46:25 +0300

    uploader tests

---
+++ internal/agent/uploader/uploader_test.go
@@ -0,0 +1,68 @@
+package uploader
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+var gaugeMetrics = func() map[string]float64 {
+	return map[string]float64{
+		"metric1": 0.1,
+		"metric2": 0.2,
+	}
+}
+
+var counterMetrics = func() map[string]int64 {
+	return map[string]int64{
+		"metric1": 1,
+		"metric2": 2,
+	}
+}
+
+func TestNewUploader(t *testing.T) {
+	gaugeFunc := func() map[string]float64 {
+		return nil
+	}
+
+	counterFunc := func() map[string]int64 {
+		return nil
+	}
+
+	uploader := NewUploader(gaugeFunc, counterFunc)
+
+	if reflect.ValueOf(uploader.gaugeMetricsFunc).Pointer() != reflect.ValueOf(gaugeFunc).Pointer() {
+		t.Error("Gauge metrics function not initialized correctly.")
+	}
+
+	if reflect.ValueOf(uploader.counterMetricsFunc).Pointer() != reflect.ValueOf(counterFunc).Pointer() {
+		t.Error("Counter metrics function not initialized correctly.")
+	}
+}
+
+func TestUploader_SendGaugeMetrics(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			t.Errorf("Expected POST request, got %s", r.Method)
+		}
+	}))
+	defer ts.Close()
+
+	uploader := NewUploader(gaugeMetrics, counterMetrics)
+
+	uploader.SendGaugeMetrics(gaugeMetrics())
+}
+
+func TestUploader_SendCounterMetrics(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			t.Errorf("Expected POST request, got %s", r.Method)
+		}
+	}))
+	defer ts.Close()
+
+	uploader := NewUploader(gaugeMetrics, counterMetrics)
+
+	uploader.SendCounterMetrics(counterMetrics())
+}
